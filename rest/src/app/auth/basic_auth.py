1023601af7aeaab3def99de12db7318a05ab5a82
WojciechCebula <wojtekcebulaa@gmail.com>
2021-12-28 01:50:30 +0100

    There is already a handler for 401 codes

--- rest/src/app/auth/basic_auth.py
+++ rest/src/app/auth/basic_auth.py
@@ -17,17 +17,5 @@ def verify_password(username, password):
 
 
 @BASIC_AUTH.error_handler
-@response_wrapper
 def bad_basic_auth(code):
-    if BASIC_AUTH.get_auth() is None:
-        error = ResponseError(
-            description='The server could not verify that you are authorized to access the URL requested. No credentials supplied.', 
-            name='Authorization Missing'
-        )
-        return ResponseData(error=error, code=code)
-
-    error = ResponseError(
-        description='The server could not verify that you are authorized to access the URL requested. Wrong credentials supplied (e.g. a bad password).',
-        name='Unauthorized Access'
-    )
-    return ResponseData(error=error, code=code)
+    abort(code)
