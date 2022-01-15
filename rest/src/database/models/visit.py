5416ac1aefd58276869d482a62ad7773929d4be6
JakubMakarewicz <makaron@KOMPUTER>
2022-01-15 16:58:24 +0100

    int -> str

--- rest/src/database/models/visit.py
+++ rest/src/database/models/visit.py
@@ -7,7 +7,7 @@ class Visit(Model):
     _id: str = field(init=True, default=None)
     visit_start: str = field(init=True, default=None)
     cost_per_min: int = field(init=True, default=None)
-    visit_type: int = field(init=True, default=None)
+    visit_type: str = field(init=True, default=None)
     user: str = field(init=True, default=None)
     total_cost: int = field(init=True, default=None)
     visit_end: str = field(init=True, default=None)
