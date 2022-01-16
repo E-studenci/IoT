1399a10863795b22ba44f9c709adc6bcda319aae
JakubMakarewicz <makaron@KOMPUTER>
2022-01-16 17:58:26 +0100

    +1 byczq

--- rest/src/database/models/user.py
+++ rest/src/database/models/user.py
@@ -1,7 +1,7 @@
 
 from dataclasses import dataclass, field
 from database.models.model import Model
-
+from database.models.visit import Visit
 @dataclass
 class User(Model):
     _id: str = field(init=True, default=None)
@@ -11,3 +11,13 @@ class User(Model):
     status: str = field(init=True, default=None)
     balance: int = field(init=True, default=None)
     card: str = field(init=True, default=None) ## TODO: add visit to user
+    current_visit: Visit = field(init=True, default=None)
+
+    @classmethod
+    def from_dict(cls, source: dict): 
+        ret_user = super().from_dict(source)
+        if "current_visit" not in source:
+            return ret_user
+        visit = Visit.from_dict(source["current_visit"])
+        ret_user.current_visit = visit
+        return ret_user
