d9730230b84d5989627ea9c87e3714c4978aab88
PANtroll <64544417+PANtroll@users.noreply.github.com>
2022-01-27 19:37:30 +0100

    Update index.js

--- webpage/src/router/index.js
+++ webpage/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import CurrentClients from '../views/CurrentClients.vue'
 import AllClients from '../views/AllClients.vue'
+import AddClient from '../views/AddClient.vue'
+import EditClient from '../views/EditClient.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +23,16 @@ const routes = [
     path: '/allClients',
     name: 'AllClients',
     component: AllClients
+  },
+  {
+    path: '/addClient',
+    name: 'AddClient',
+    component: AddClient
+  },
+  {
+    path: '/editClient',
+    name: 'EditClient',
+    component: EditClient
   }
 ]
 
